66aa98528b79b5e03a96c27d4bd0cef8fe0245f8
HoHuuAn <anho.work@gmail.com>
2023-12-25 00:33:27 +0700

    add_products

--- src/main/java/com/JavaTech/HeadQuarter/controller/ProductController.java
+++ src/main/java/com/JavaTech/HeadQuarter/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.JavaTech.HeadQuarter.controller;
 
 
+import com.JavaTech.HeadQuarter.model.Branch;
+import com.JavaTech.HeadQuarter.model.Brand;
+import com.JavaTech.HeadQuarter.model.Product;
 import com.JavaTech.HeadQuarter.model.QuantityProduct;
 import com.JavaTech.HeadQuarter.service.BranchService;
+import com.JavaTech.HeadQuarter.service.BrandService;
 import com.JavaTech.HeadQuarter.service.ProductService;
 import com.JavaTech.HeadQuarter.service.QuantityProductService;
+import com.JavaTech.HeadQuarter.utils.BarcodeUtil;
+import com.JavaTech.HeadQuarter.utils.ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,13 +19,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/product")
 public class ProductController {
+
     @Autowired
     private ProductService productService;
 
@@ -29,8 +40,11 @@ public class ProductController {
     @Autowired
     private BranchService branchService;
 
+    @Autowired
+    private BrandService brandService;
+
     @GetMapping(value = "/list")
-    public String showBranch(Model model){
+    public String showAll(Model model){
         model.addAttribute("listProducts", productService.listDTO(productService.listAll()));
         return "/product/page-list-products";
     }
@@ -52,5 +66,51 @@ public class ProductController {
         }
         Map<String, Object> response = new HashMap<>();
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "/add")
+    public String showViewAdd(Model model){
+        model.addAttribute("listBrand", brandService.listAll());
+        return "/product/page-add-products";
+    }
+
+    @PostMapping(value = "/add")
+    public String addProduct(@RequestParam(name = "name") String name,
+                             @RequestParam(name = "importPrice") int importPrice,
+                             @RequestParam(name = "retailPrice") int retailPrice,
+                             @RequestParam(name = "quantity") int quantity,
+                             @RequestParam(name = "barCode") String barCode,
+                             @RequestParam(name = "image") MultipartFile image,
+                             @RequestParam(name = "brand") String brand,
+                             @RequestParam(name = "description") String description) throws IOException {
+        //save product
+        Brand brand_org = brandService.findByName(brand);
+
+        Product product = Product.builder()
+                .name(name)
+                .importPrice(importPrice)
+                .retailPrice(retailPrice)
+                .image(image.getBytes())
+                .createdAt(new Date())
+                .barCode(barCode)
+                .imageBarCode(ImageUtil.convertToBase64(BarcodeUtil.generateCodeBarcode(barCode, name)))
+                .brand(brand_org)
+                .totalSales(0)
+                .description(description)
+                .build();
+        brand_org.getProducts().add(product);
+        brandService.addOrSave(brand_org);
+        productService.saveOrUpdate(product);
+
+        //quantity
+        for (Branch branch : branchService.listAll()) {
+            QuantityProduct quantityProduct = QuantityProduct.builder()
+                        .branch(branch)
+                        .product(product)
+                        .quantity(0)
+                        .build();
+            quantityProductService.saveOrUpdate(quantityProduct);
+        }
+        return "redirect:/product/list";
     }
 }
